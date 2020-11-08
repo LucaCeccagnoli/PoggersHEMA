ed37c1f0f1b9adf35cc03b416b1caade05d08465
Dexef <jempest548@gmail.com>
2020-11-08 16:44:38 +0100

    autismo BONG BANG BANG BONG DING

--- articles/api/urls.py
+++ articles/api/urls.py
@@ -3,9 +3,9 @@ from articles.api.views import *
 
 urlpatterns = [
     # lista completa articoli
-    path("article-list/", ArticleListAPIView.as_view()), 
+    path("article-list/", ArticleListAPIView.as_view()),
     # articoli filtrati per categorie
-    path("article-list/<str:category>", ArticleListAPIView.as_view()), 
+    path("article-list/<str:category>", ArticleListAPIView.as_view()),
     path("article-list/<str:category>/<str:sub_category>", ArticleListAPIView.as_view()),
     path("article-detail/<int:pk>", ArticleDetailAPIView.as_view()),    # ottieni/modifica singolo articolo
     path("article-detail/create/", ArticleCreateAPIView.as_view()),     #crea un nuovo articolo
@@ -13,13 +13,15 @@ urlpatterns = [
                                                                         # con chiave primaria per ottenerne un singolo
 
     # ottiene gli oggetti nel carrello di un utente data la sua chiave primaria
-    path("orders/<int:pk>", OrderDetailApiView.as_view(), name = "order-list"),  
+    path("orders/<int:pk>", OrderDetailApiView.as_view(), name = "order-list"),
     # conferma l'ordine nel carrello di un utente e genera il rispettivo shipment
     path("confirm-order/<int:pk>", ConfirmOrderAPIView.as_view(), name = "confirm-order"),
 
-    path("shipment-list/", ShipmentListAPIView.as_view() ),# lista completa shipment
-    path("shipment-list/<int:pk>", ShipmentListAPIView.as_view()), # lista shipment effettuati da un utente
-    path("shipment-detail/<int:pk>", ShipmentDetailAPIView.as_view()), # dettaglio singolo shipment
+    # lista completa shipment
+    path("shipment-list/", ShipmentListAPIView.as_view() ),
+
+    # lista shipment effettuati da un utente
+    path("shipment-list/<int:pk>", ShipmentListAPIView.as_view()),
 
     path("add-to-cart/", AddItemAPIView.as_view(), name = "add-to-cart"),   # aggiungi articolo al carrello
 
